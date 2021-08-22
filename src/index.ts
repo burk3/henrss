6385186e3855cd7d1bd3007868f3839f00f38e8a
Burke Cates <burke.cates@gmail.com>
2021-08-22 15:00:15 -0700

    forgot to update the routes, lol.

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,23 @@ const app = express();
 
 app.use(compression());
 
-app.get("/feed.rss", async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("/newlyMinted/{feed.rss,feed.json,atom.xml}");
+});
+
+app.get("/newlyMinted/feed.rss", async (req, res) => {
   const feed = await genNewlyMintedFeed();
   res.set("Content-Type", "application/rss+xml");
   res.send(feed.rss2());
 });
 
-app.get("/atom.xml", async (req, res) => {
+app.get("/newlyMinted/atom.xml", async (req, res) => {
   const feed = await genNewlyMintedFeed();
   res.set("Content-Type", "application/atom+xml");
   res.send(feed.atom1());
 });
 
-app.get("/feed.json", async (req, res) => {
+app.get("/newlyMinted/feed.json", async (req, res) => {
   const feed = await genNewlyMintedFeed();
   res.set("Content-Type", "application/json");
   res.send(feed.json1());
